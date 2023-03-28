36ba4616c23d27d59bbf0e4c0c1f36c6c80c19bd
Your Name <you@example.com>
2023-03-28 21:20:29 +0200

    add commands

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,88 @@
 use std::{env, process::exit};
 
+struct Id(String);
+
+enum Command {
+    Add(String),
+    Get(Id),
+    Update(Id, String),
+    Delete(Id),
+    Help,
+}
+
 enum ArgsError {
-    NoArg(String),
-    NotValid(String),
+    NoCommand,
+    NonValidCommand(String),
+    UpdateWithoutNewValue,
+    NotEnoughArgument,
 }
 
 fn handle_errors(error: ArgsError) {
     match error {
-        ArgsError::NotValid(error_message) => print!("Argument Error: {}", error_message),
-        ArgsError::NoArg(error_message) => print!("Argument Error: {}", error_message),
+        ArgsError::NoCommand => {
+            print!("No Command has been specified! Try: add, get, update, delete.")
+        }
+        ArgsError::NonValidCommand(error_message) => print!(
+            "Command do not exists: {}! Try: add, get, update, delete.",
+            error_message
+        ),
+        ArgsError::UpdateWithoutNewValue => print!("Please specify the new value for that todo!"),
+        ArgsError::NotEnoughArgument => print!("Command need more parameter."),
     }
 
-    exit(1);
-}
-
-struct Command {
-    command: String,
-    input: String,
+    exit(1)
 }
 
 fn validate_arguments(args: Vec<String>) -> Result<Command, ArgsError> {
     if args.len() < 2 {
-        return Err(ArgsError::NoArg("No argument was given!".to_string()));
+        return Err(ArgsError::NoCommand);
     }
 
     if args.len() < 3 {
-        return Err(ArgsError::NotValid("Argument is not valid!".to_string()));
+        return match args[1].as_str() {
+            "help" => Ok(Command::Help),
+            "--help" => Ok(Command::Help),
+            "-h" => Ok(Command::Help),
+            "add" => Err(ArgsError::NotEnoughArgument),
+            "get" => Err(ArgsError::NotEnoughArgument),
+            "update" => Err(ArgsError::NotEnoughArgument),
+            "delete" => Err(ArgsError::NotEnoughArgument),
+            _ => Err(ArgsError::NonValidCommand(args[1].clone())),
+        };
     }
 
-    let command = args[1].clone();
-    let input = args[2].clone();
+    return match args[1].as_str() {
+        "add" => Ok(Command::Add(args[2].clone())),
+        "get" => Ok(Command::Get(Id(args[2].clone()))),
+        "update" => {
+            if args.len() < 4 {
+                return Err(ArgsError::UpdateWithoutNewValue);
+            }
 
-    return Ok(Command { command, input });
+            return Ok(Command::Update(Id(args[2].clone()), args[3].clone()));
+        }
+        "delete" => Ok(Command::Delete(Id(args[2].clone()))),
+        _ => Err(ArgsError::NonValidCommand(args[2].clone())),
+    };
+}
+
+fn handle_command(command: Command) {
+    match command {
+        Command::Add(input) => println!("{}", input),
+        Command::Get(Id(id)) => println!("{}", id),
+        Command::Update(Id(id), input) => println!("{}, {}", id, input),
+        Command::Delete(Id(id)) => println!("{}", id),
+        Command::Help => println!("{}", "help"),
+    }
+
+    exit(0)
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     match validate_arguments(args) {
-        Ok(command) => println!("{}, {}", command.command, command.input),
-        Err(error) => handle_errors(error)
+        Ok(command) => handle_command(command),
+        Err(error) => handle_errors(error),
     };
 }
